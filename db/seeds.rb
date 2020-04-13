7806fe9d45b37321623f400c7baef31fd6699c37
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-04-13 16:25:15 +0200

    adding seeds for subcategories

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ tanning.photo.attach(io:file, filename:'tanning.jpg', content_type: 'image/jpg')
 file1 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1586180075/waterlily/massage.jpg')
 massage = Category.create(
   name: "Massage",
-  description: "Indulge your senses with a relaxing massage. From Thai Massages to Hot Stone, we offer a variety of massages to help ease tension and make you feel rejuvenated."
+  description: "Indulge your senses with a relaxing massage. The effective and enjoyable way to improve skin health, texture and circulation whilst relieving stress and tension."
 )
 
 massage.photo.attach(io:file1, filename:'massage.jpg', content_type: 'image/jpg')
@@ -90,5 +90,133 @@ thaimassage = Subcategory.create(
   description: "Thai Massage with Onsri",
   category_id: Category.find_by_name("Massage").id
 )
+
+#swedish massage
+swedish = Subcategory.create(
+  name: "Swedish",
+  description: "Deep Tissue Massage",
+  category_id: Category.find_by_name("Massage").id
+)
+
+#aromatherapy massage
+aromatherapy = Subcategory.create(
+  name: "Aromatherapy",
+  description: "Relaxation Massage",
+  category_id: Category.find_by_name("Massage").id
+)
+
+#hotstone massage
+hotstone = Subcategory.create(
+  name: "Hot Stone",
+  description: "Soothing massage using hot volcanic stones",
+  category_id: Category.find_by_name("Massage").id
+)
+
+#packages
+packages = Subcategory.create(
+  name: "Packages",
+  description: "Value Packages",
+  category_id: Category.find_by_name("Massage").id
+)
+
+#tanning
+verticaltanning = Subcategory.create(
+  name: "Vertical Tanning",
+  description: "We pride ourselves on our professional advice and guidance when using tanning equipment. To maintain a healthy tan, usually 1-2 sessions are suffice.",
+  category_id: Category.find_by_name("Tanning").id
+)
+
+#manicures
+manicures = Subcategory.create(
+  name: "Manicures",
+  description: "Look after your hands",
+  category_id: Category.find_by_name("Hands, Feet & Nails").id
+)
+
+#shellac
+shellac = Subcategory.create(
+  name: "Shellac Overlays",
+  description: "On like a polish, wears like a gel",
+  category_id: Category.find_by_name("Hands, Feet & Nails").id
+)
+
+#shellac
+gellux = Subcategory.create(
+  name: "Gellux",
+  description: "On like a polish, wears like a gel",
+  category_id: Category.find_by_name("Hands, Feet & Nails").id
+)
+
+#pedicures
+pedicures = Subcategory.create(
+  name: "Pedicures",
+  description: "Give your feet the care they deserve",
+  category_id: Category.find_by_name("Hands, Feet & Nails").id
+)
+
+#packages
+packages2 = Subcategory.create(
+  name: "Packages",
+  description: "Value Packages",
+  category_id: Category.find_by_name("Hands, Feet & Nails").id
+)
+
+#facials
+packages2 = Subcategory.create(
+  name: "Facials",
+  description: "Leave your skin feeling smooth and fresh",
+  category_id: Category.find_by_name("Hands, Feet & Nails").id
+)
+
+#facewaxing
+face = Subcategory.create(
+  name: "Face Waxing",
+  description: "Using a gentle creme for sensitive skin",
+  category_id: Category.find_by_name("Waxing").id
+)
+
+#bodywaxing
+body = Subcategory.create(
+  name: "Body Waxing",
+  description: "Using a gentle creme for sensitive skin",
+  category_id: Category.find_by_name("Waxing").id
+)
+
+#arasys
+arasys = Subcategory.create(
+  name: "Arasys",
+  description: "Each 17 minute session is equivalent to 250 sit-ups or 300 buttock raises. Widely known as the best faradic system",
+  category_id: Category.find_by_name("Body Contour").id
+)
+
+#hartour
+hartour = Subcategory.create(
+  name: "Hartour Ultrasound",
+  description: "The effective sollution to cellulite. Breaks down the hardened deposits and deep lymphatic massage flushes toxins from the body",
+  category_id: Category.find_by_name("Body Contour").id
+)
+
+#xfit
+xfit = Subcategory.create(
+  name: "X-Fit Vibration Plate",
+  description: "10 minute session burns fat, helps you lose weight, reduces cellulite, increases circulation and builds muscle tone.",
+  category_id: Category.find_by_name("Body Contour").id
+)
+
+#tintingperming
+tintingperming = Subcategory.create(
+  name: "Tinting & Perming",
+  description: "Please note that a patch test is required at least 24 hours prior to tinting and perming",
+  category_id: Category.find_by_name("Body Contour").id
+)
+
+#cosmetic
+cosmetic = Subcategory.create(
+  name: "Cosmetic Treatments",
+  description: "Please contact us for a consulation",
+  category_id: Category.find_by_name("Cosmetic Treatments").id
+)
+
+
 
 puts "Completed subcategory"
