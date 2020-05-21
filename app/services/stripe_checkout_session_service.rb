5f79907f07e6bbf3a7a9655cab3810d3440eb6ba
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-05-21 10:52:39 +0200

    -adding email to user from stripe webhook

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,5 +2,7 @@ class StripeCheckoutSessionService
   def call(event)
     voucher = Voucher.find_by(checkout_session_id: event.data.object.id)
     voucher.update(state: 'paid')
+    @user = voucher.user
+    UserNotifierMailer.voucher_purchase_email(@user).deliver
   end
 end
