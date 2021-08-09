c9a892d51d2dbc9506a23bc159e2ab3ae7a496cd
Abby Hudson <abbyeleanorhudson@gmail.com>
2021-08-09 10:39:21 +0100

    fix typo

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,7 +2,7 @@ class StripeCheckoutSessionService
   def call(event)
     @voucher = Voucher.find_by(checkout_session_id: event.data.object.id)
     @voucher.update(state: 'paid')
-    @user = voucher.user
+    @user = @voucher.user
     UserNotifierMailer.voucher_purchase_email(@user, @voucher).deliver
     UserNotifierMailer.notify_waterlily_email(@user, @voucher).deliver
   end
