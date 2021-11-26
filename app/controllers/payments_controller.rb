afd2560b94a791ecdd74e58c33d6c3ed2487f349
Abby Hudson <abbyeleanorhudson@gmail.com>
2021-11-26 15:26:51 +0000

    make sure user is authenticated before buying voucher

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,6 @@
 class PaymentsController < ApplicationController
-  before_action :set_categories
+  before_action :set_categories, :authenticate_user!
+  
   def new
     @voucher = current_user.vouchers.where(state: "pending").find(params[:voucher_id])
     authorize @voucher
