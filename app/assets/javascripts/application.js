0a70d14e69874444fc84a807a8c14db4aacafa9f
abbyhudson13 <55158466+abbyhudson13@users.noreply.github.com>
2022-12-30 11:23:52 +0000

    update-ruby-version): try and fix jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
 //= require jquery
+//= require jquery.turbolinks
+//= require datatables
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require datatables
 //= require_tree .
 
 document.addEventListener('turbolinks:load', () => {
